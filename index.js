d85fa3dbfa01af728c387359d2a0a540a52a5039
Zunair Khan <121635619+zunairkhan313@users.noreply.github.com>
2023-11-02 19:31:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const userRoute = require("./Routes/user")
+
+app.use(morgan('tiny'))
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://blog:blog@cluster0.rihhmnm.mongodb.net/").then(console.log('Mongodb conneted')).catch((err)=>console.log(err))
+
+
+app.get('/',(req,res)=>{
+    res.send(['zunair','khan'])
+})
+
+
+app.use('/user',userRoute)
+
+app.listen(3000,()=>{
+    console.log("App is running on port 3000");
+})
